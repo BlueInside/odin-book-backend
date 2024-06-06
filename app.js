c62ace620edf221027beac6dffe7a75d3dca4535
Karol <kpulawski13@gmail.com>
2024-06-06 11:30:57 +0100

    connect to the database in app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// connect to the database
+require('./config/mongo');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
