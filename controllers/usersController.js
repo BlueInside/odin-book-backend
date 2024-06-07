0077b9effbc1eadd2bedbac2d034d8621b77018d
Karol <kpulawski13@gmail.com>
2024-06-07 19:00:36 +0100

    implement GET /users/:userId route handler

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/user');
+const Post = require('../models/post');
 const mongoose = require('mongoose');
 
 const getAllUsers = asyncHandler(async (req, res, next) => {
@@ -44,7 +45,20 @@ const getUser = asyncHandler(async (req, res, next) => {
 const getUserPosts = asyncHandler(async (req, res, next) => {
   // Add pagination and filtering?
   const { userId } = req.params;
-  res.status(200).send(`GET /users/${userId}/posts not implemented`);
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    return res.status(400).json({ error: 'Invalid user ID.' });
+  }
+
+  const posts = await Post.find({ user: userId }).sort({ createdAt: 1 });
+
+  if (!posts.length) {
+    return res.status(404).json({ error: `This user has no posts yet.` });
+  }
+
+  const totalPosts = await Post.countDocuments({ user: userId });
+
+  return res.status(200).json({ posts: posts, totalPosts: totalPosts });
 });
 
 const getUserLikes = asyncHandler(async (req, res, next) => {
