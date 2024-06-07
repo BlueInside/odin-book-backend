ab602e2a97f20cc55ee0fd3d9c128a05ef9a78e6
Karol <kpulawski13@gmail.com>
2024-06-07 11:58:25 +0100

    Test searching query and if users return 404

--- tests/user.test.js
+++ tests/user.test.js
@@ -30,5 +30,32 @@ describe('GET /users', () => {
     expect(response.body).toHaveProperty('users');
   });
 
+  it('Should return users matching the search query', async () => {
+    const mockUsers = [
+      { firstName: 'John', lastName: 'Doe', fullName: 'John Doe' },
+    ];
+    User.find.mockImplementation(() => ({
+      sort: () => ({
+        limit: () => Promise.resolve(mockUsers),
+      }),
+    }));
+
+    const response = await request(app).get('/users').query('q=John');
+    expect(response.status).toBe(200);
+    expect(response.body.users).toEqual(mockUsers);
+  });
+
+  it('Should return 404 if no users are found', async () => {
+    User.find.mockImplementation(() => ({
+      sort: () => ({
+        limit: () => Promise.resolve([]),
+      }),
+    }));
+
+    const response = await request(app).get('/users');
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty('error', 'Users not found.');
+  });
+
   it(`Should get user by its id and return the user info`, async () => {});
 });
