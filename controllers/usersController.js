517d36212093581fc2880545b4e1be4d2c7c5779
Karol <kpulawski13@gmail.com>
2024-06-07 11:31:48 +0100

    Implement getAllUsers handler

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,31 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/user');
 
 const getAllUsers = asyncHandler(async (req, res, next) => {
-  res.status(200).send('GET /users not implemented');
+  // return list of all users if no search query
+  const { q } = req.query;
+  const query = {};
+
+  if (q) {
+    query.$or = [
+      { firstName: { $regex: q, $options: 'i' } },
+      { lastName: { $regex: q, $options: 'i' } },
+    ];
+  }
+
+  const users = await User.find(query, 'firstName lastName')
+    .sort({ username: 1 })
+    .limit(10);
+
+  if (!users.length) {
+    return res.status(404).json({ error: 'Users not found.' });
+  }
+  return res.status(200).json({ users: users });
 });
 
 const getUser = asyncHandler(async (req, res, next) => {
   const { userId } = req.params;
+
   res.status(200).send(`GET /users/${userId} not implemented`);
 });
 
