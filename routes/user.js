a2d273cc20f3090fcb77d657b7ea18903f1a4ea5
Karol <kpulawski13@gmail.com>
2024-06-08 13:52:07 +0100

    Add getUserPostsValidation to middleware chain

--- routes/user.js
+++ routes/user.js
@@ -4,11 +4,16 @@ const userController = require('../controllers/usersController');
 const {
   getAllUsersValidation,
   getUserValidation,
+  getUserPostsValidation,
 } = require('../lib/userValidation');
 
 router.get('/', getAllUsersValidation(), userController.getAllUsers);
 router.get('/:userId', getUserValidation(), userController.getUser);
-router.get('/:userId/posts', userController.getUserPosts);
+router.get(
+  '/:userId/posts',
+  getUserPostsValidation(),
+  userController.getUserPosts
+);
 router.get('/:userId/likes', userController.getUserLikes);
 router.post('/', userController.createUser);
 router.put('/:userId', userController.updateUser);
