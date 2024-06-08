a89a8d4f4db68aaee9d5d7541dc2c9e98b7cdaed
Karol <kpulawski13@gmail.com>
2024-06-08 14:03:29 +0100

    Adjust test error check

--- tests/user.test.js
+++ tests/user.test.js
@@ -219,7 +219,10 @@ describe('GET /users/:userId/likes', () => {
 
     const response = await request(app).get('/users/123/likes');
     expect(response.status).toBe(400);
-    expect(response.body).toHaveProperty('error', 'Invalid user ID.');
+    expect(response.body.errors[0]).toHaveProperty(
+      'msg',
+      'User ID must be a valid MongoDB ObjectId.'
+    );
   });
 
   it('Should return 404 if no liked posts are found for the user', async () => {
