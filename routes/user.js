fb14411d773a2be336d179d047809e1d78fa4542
Karol <kpulawski13@gmail.com>
2024-06-08 14:03:18 +0100

    Add whitespace use userLikesValidation

--- routes/user.js
+++ routes/user.js
@@ -5,18 +5,29 @@ const {
   getAllUsersValidation,
   getUserValidation,
   getUserPostsValidation,
+  getUserLikesValidation,
 } = require('../lib/userValidation');
 
 router.get('/', getAllUsersValidation(), userController.getAllUsers);
+
 router.get('/:userId', getUserValidation(), userController.getUser);
+
 router.get(
   '/:userId/posts',
   getUserPostsValidation(),
   userController.getUserPosts
 );
-router.get('/:userId/likes', userController.getUserLikes);
+
+router.get(
+  '/:userId/likes',
+  getUserLikesValidation(),
+  userController.getUserLikes
+);
+
 router.post('/', userController.createUser);
+
 router.put('/:userId', userController.updateUser);
+
 router.delete('/:userId', userController.deleteUser);
 
 module.exports = router;
