1b99dba8d62d452d6fded02f4fe84aca5219bd22
Karol <kpulawski13@gmail.com>
2024-06-08 13:12:49 +0100

    Implement getProfile controller

--- controllers/profilesController.js
+++ controllers/profilesController.js
@@ -1,7 +1,16 @@
 const asyncHandler = require('express-async-handler');
+const Profile = require('../models/profile');
 
 const getProfile = asyncHandler(async (req, res, next) => {
-  return res.status(200).send('Get profile controller is NOT IMPLEMENTED');
+  const { userId } = req.session.id; // Get user id from authenticate middleware.
+
+  const profile = await Profile.find({ user: userId });
+
+  if (!profile) {
+    return res.status(404).send({ error: `Doesn't have profile information.` });
+  }
+
+  return res.status(200).json({ profile: profile });
 });
 
 const updateProfile = asyncHandler(async (req, res, next) => {
