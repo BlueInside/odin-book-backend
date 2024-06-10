18d5c1e6e5ab7a50c7ee6a0da3ed39ebd82f83f7
Karol <kpulawski13@gmail.com>
2024-06-10 08:14:32 +0100

    Adjust user model to fit github auth

--- models/user.js
+++ models/user.js
@@ -9,10 +9,10 @@ const options = {
 
 const userSchema = new Schema(
   {
+    githubId: { type: String, required: true, unique: true }, // Ensure uniqueness
     firstName: { type: String, required: true, index: true },
-    lastName: { type: String, index: true },
-    password: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    lastName: { type: String, index: true, default: '' }, // Set default if empty
+    email: { type: String, unique: true, sparse: true }, // Make email sparse for uniqueness only when it is provided
     profilePicture: { type: String },
     bio: { type: String },
     isActive: { type: Boolean, default: true },
