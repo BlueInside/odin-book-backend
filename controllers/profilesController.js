fd4bcdf7b7e28f15bb6a3218267589993533db2d
Karol <kpulawski13@gmail.com>
2024-06-10 18:12:27 +0100

    Implement create profile controller

--- controllers/profilesController.js
+++ controllers/profilesController.js
@@ -13,13 +13,46 @@ const getProfile = asyncHandler(async (req, res, next) => {
   return res.status(200).json({ profile: profile });
 });
 
-const updateProfile = asyncHandler(async (req, res, next) => {
-  // Only authenticated users
-  const { id } = req.params;
+const createProfile = asyncHandler(async (req, res, next) => {
+  const { userId, birthday, interests, hobby } = req.body;
 
-  return res
-    .status(200)
-    .send(`Update profile ${id} controller is NOT IMPLEMENTED`);
+  if (req.user.role !== 'admin' && req.user.id !== userId) {
+    return res
+      .status(403)
+      .json({ error: 'You do not have permission to delete this profile.' });
+  }
+
+  const newProfile = new Profile({
+    user: userId,
+    birthday: new Date(birthday),
+    interests,
+    hobby,
+  });
+
+  const createdProfile = await newProfile.save();
+
+  return res.status(201).json({ profile: createdProfile });
+});
+
+const updateProfile = asyncHandler(async (req, res, next) => {
+  const { profileId } = req.params;
+  const { birthday, interests, hobby } = req.body;
+
+  const updates = {};
+
+  if (birthday !== undefined) updates.birthday = new Date(birthday);
+  if (interests !== undefined) updates.interests = interests;
+  if (hobby !== undefined) updates.hobby = hobby;
+
+  const profile = await Profile.findByIdAndUpdate(profileId, updates, {
+    new: true,
+  });
+
+  if (!profile) {
+    return res.status(404).json({ message: 'Profile not found!' });
+  }
+
+  return res.status(200).json({ profile });
 });
 
 const deleteProfile = asyncHandler(async (req, res, next) => {
@@ -32,6 +65,7 @@ const deleteProfile = asyncHandler(async (req, res, next) => {
 
 module.exports = {
   getProfile,
+  createProfile,
   updateProfile,
   deleteProfile,
 };
