c0f4fe2744e78037789b658971a5f97bf5712932
Karol <kpulawski13@gmail.com>
2024-06-10 11:34:13 +0100

    Adjust authenticate routes to work with github and return jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const authenticationController = require('../controllers/authenticationController');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const { generateToken } = require('../config/jwt');
 
-router.post('/login', authenticationController.login);
-router.post('/logout', authenticationController.logout);
+router.get(
+  '/github',
+  passport.authenticate('github', { scope: ['user:email'] })
+);
+
+router.get(
+  '/github/callback',
+  passport.authenticate('github', {
+    session: false,
+    failureRedirect: '/login',
+  }),
+
+  function (req, res) {
+    if (!req.user) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+    const token = generateToken(req.user);
+
+    return res.status(200).json({ success: true, token: token });
+  }
+);
+
 router.post('/register', authenticationController.register);
 
 module.exports = router;
