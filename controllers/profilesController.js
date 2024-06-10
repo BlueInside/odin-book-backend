c31147012fb25bc4f680bed964a78b36d3208684
Karol <kpulawski13@gmail.com>
2024-06-10 13:15:00 +0100

    Implement getProfile route controller

--- controllers/profilesController.js
+++ controllers/profilesController.js
@@ -1,12 +1,22 @@
 const asyncHandler = require('express-async-handler');
+const Profile = require('../models/profile');
 
 const getProfile = asyncHandler(async (req, res, next) => {
-  return res.status(200).send('Get profile controller is NOT IMPLEMENTED');
+  const { userId } = req.params;
+
+  const profile = await Profile.findOne({ user: userId });
+
+  if (!profile) {
+    return res.status(404).json({ message: 'Profile not found!' });
+  }
+
+  return res.status(200).json({ profile: profile });
 });
 
 const updateProfile = asyncHandler(async (req, res, next) => {
   // Only authenticated users
   const { id } = req.params;
+
   return res
     .status(200)
     .send(`Update profile ${id} controller is NOT IMPLEMENTED`);
