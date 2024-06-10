466e4f9e87d945d5fc0837f626ac805c8e5401c2
Karol <kpulawski13@gmail.com>
2024-06-10 11:56:19 +0100

    change username to firstName

--- config/jwt.js
+++ config/jwt.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const generateToken = (user) => {
   return jwt.sign(
-    { id: user.id, username: user.username, role: user.role },
+    { id: user.id, firstName: user.firstName, role: user.role },
     process.env.JWT_SECRET,
     { expiresIn: '24h' }
   );
