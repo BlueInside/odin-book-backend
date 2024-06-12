cc9f49c86d0f83ce715d0b1a29714501f2b9aae0
Karol <kpulawski13@gmail.com>
2024-06-12 15:15:46 +0100

    Add authenticate and validation for GET /posts

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/postsController');
+const { getAllPostsValidation } = require('../lib/postsValidation');
+const { authenticateToken } = require('../config/jwt');
 
-router.get('/', postsController.getAllPosts);
+router.get(
+  '/',
+  authenticateToken,
+  getAllPostsValidation(),
+  postsController.getAllPosts
+);
+
 router.get('/:postId', postsController.getPost);
 router.get('/:postId/likes', postsController.getPostLikes);
 router.get('/:postId/comments', postsController.getPostComments);
