b39be88eacae0c3aeca1383cbf80eaa8c4ce91a3
Karol <kpulawski13@gmail.com>
2024-06-14 19:00:00 +0100

    createLike route controller

--- controllers/likesController.js
+++ controllers/likesController.js
@@ -1,7 +1,28 @@
 const asyncHandler = require('express-async-handler');
+const Like = require('../models/like');
+const Post = require('../models/post');
 
 const createLike = asyncHandler(async (req, res, next) => {
-  res.status(201).send(`POST /likes/ not implemented`);
+  const { postId, userId } = req.body;
+
+  const post = await Post.findById(postId);
+  if (!post) {
+    return res.status(404).json({ message: 'Post not found' });
+  }
+
+  const existingLike = await Like.findOne({ post: postId, user: userId });
+  if (existingLike) {
+    return res
+      .status(400)
+      .json({ message: 'You have already liked this post' });
+  }
+
+  const like = new Like({ post: postId, user: userId });
+  await like.save();
+
+  await post.incrementLikes();
+
+  res.status(201).json({ message: 'Like created successfully' });
 });
 
 const deleteLike = asyncHandler(async (req, res, next) => {
