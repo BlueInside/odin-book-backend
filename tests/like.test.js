ea02626f3c2b8a6cd9dc9ad623b9678d0d753262
Karol <kpulawski13@gmail.com>
2024-06-17 20:25:26 +0100

    Test DELETE /like

--- tests/like.test.js
+++ tests/like.test.js
@@ -6,7 +6,6 @@ const Post = require('../models/post');
 const mongoose = require('mongoose');
 const app = express();
 const { generateToken } = require('../config/jwt');
-const { expect } = require('@jest/globals');
 
 // Mocks
 jest.mock('../models/post'); // Mock the Post model
@@ -56,7 +55,7 @@ describe('POST /like', () => {
       .send({ postId, userId });
 
     expect(response.status).toBe(201);
-    expect(response.body.message).toBe('Like created successfully');
+    expect(response.body.message).toBe('Like created successfully.');
     expect(mockIncrementLikeCount).toHaveBeenCalled();
   });
 
@@ -94,6 +93,93 @@ describe('POST /like', () => {
       .send({ postId, userId });
 
     expect(response.status).toBe(400);
-    expect(response.body.message).toBe('You have already liked this post');
+    expect(response.body.message).toBe('You have already liked this post.');
+  });
+});
+
+describe('DELETE /like', () => {
+  const userId = new mongoose.Types.ObjectId().toString();
+  const likeId = new mongoose.Types.ObjectId().toString();
+  const postId = new mongoose.Types.ObjectId().toString();
+  const userDataPayload = {
+    id: userId,
+    firstName: 'Karol',
+    role: 'admin',
+  };
+
+  const token = generateToken(userDataPayload);
+
+  it('Should delete like successfully', async () => {
+    const like = { user: userId, post: postId };
+    const post = {
+      _id: postId,
+      content: 'Test Post',
+      author: userId,
+      likesCount: 0,
+      save: jest.fn(),
+      incrementLikes: jest.fn(),
+      decrementLikes: jest.fn(),
+    };
+
+    Like.findOne.mockResolvedValue(like);
+    Like.deleteOne = jest.fn();
+    Post.findById.mockResolvedValue(post);
+
+    const response = await request(app)
+      .delete('/like')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ postId, userId });
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Like deleted successfully.');
+  });
+
+  it('Should throw 404 if no likes found', async () => {
+    const like = null;
+    Like.findOne.mockResolvedValue(like);
+
+    const response = await request(app)
+      .delete('/like')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ postId, userId });
+
+    expect(response.status).toBe(404);
+    expect(response.body.message).toBe('Like not found.');
+  });
+
+  it('Should call post decrementLikes after like deletion', async () => {
+    const like = { user: userId, post: postId };
+    const post = {
+      _id: postId,
+      content: 'Test Post',
+      author: userId,
+      likesCount: 0,
+      save: jest.fn(),
+      incrementLikes: jest.fn(),
+      decrementLikes: jest.fn(),
+    };
+
+    Like.findOne.mockResolvedValue(like);
+    Like.deleteOne = jest.fn();
+    Post.findById.mockResolvedValue(post);
+
+    const response = await request(app)
+      .delete('/like')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ postId, userId });
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Like deleted successfully.');
+    expect(post.decrementLikes).toHaveBeenCalled();
+  });
+
+  it('should return validation errors for invalid data', async () => {
+    const response = await request(app)
+      .delete('/like')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ postId: 'invalid', userId: 'invalid' });
+
+    expect(response.status).toBe(400);
+    expect(response.body.errors.length).toBeGreaterThan(0);
   });
 });
