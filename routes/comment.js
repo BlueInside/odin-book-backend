5c5ccfe339495959cf5d9450816aa17bc832fb69
Karol <kpulawski13@gmail.com>
2024-06-19 13:24:47 +0100

    Implement route POST /comments

--- routes/comment.js
+++ routes/comment.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const commentsController = require('../controllers/commentsController');
+const { authenticateToken } = require('../config/jwt');
+const { createCommentValidation } = require('../lib/commentValidation');
 
-router.post('/comments', commentsController.createComment);
+router.post(
+  '/comments',
+  authenticateToken,
+  createCommentValidation(),
+  commentsController.createComment
+);
 router.delete('/comments', commentsController.deleteComment);
 
 module.exports = router;
