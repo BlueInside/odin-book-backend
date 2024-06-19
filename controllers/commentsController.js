052408738e13b1f3588fc6556ef72e2cc5bcaf3b
Karol <kpulawski13@gmail.com>
2024-06-19 13:25:24 +0100

    implement createComment controller

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,7 +1,22 @@
 const asyncHandler = require('express-async-handler');
+const Comment = require('../models/comment');
 
 const createComment = asyncHandler(async (req, res, next) => {
-  res.status(201).send(`POST /comments/ not implemented`);
+  const authorId = req.user.id;
+  const { content, postId } = req.body;
+
+  try {
+    const comment = new Comment({
+      author: authorId,
+      post: postId,
+      content: content,
+    });
+
+    await comment.save();
+    res.status(201).json({ message: 'Comment successfully added.' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to add comment.' });
+  }
 });
 
 const deleteComment = asyncHandler(async (req, res, next) => {
