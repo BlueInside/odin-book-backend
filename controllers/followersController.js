27d42e4e2d6b766ee47ff2a793c715cd9fc11e3f
Karol <kpulawski13@gmail.com>
2024-06-19 10:38:37 +0100

    Implement controller for getFollowers

--- controllers/followersController.js
+++ controllers/followersController.js
@@ -1,10 +1,21 @@
 const asyncHandler = require('express-async-handler');
+const Follow = require('../models/follow');
 
 const getFollowers = asyncHandler(async (req, res, next) => {
   // Get user id from validation,
   const id = req.user.id;
 
-  res.status(200).json({ message: `Get for /followers is not implemented` });
+  try {
+    const followers = await Follow.find({ followed: id }).populate('follower');
+
+    if (!followers || followers.length === 0) {
+      return res.status(404).json({ message: 'No followers found.' }); // More accurate message if no followers are found
+    }
+
+    res.status(200).json({ followers: followers });
+  } catch (err) {
+    return res.status(500).json({ message: 'Error during fetching followers' });
+  }
 });
 
 const getFollowing = asyncHandler(async (req, res, next) => {
