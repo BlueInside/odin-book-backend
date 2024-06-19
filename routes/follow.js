4084242346be3386e449136be7beb8b727134633
Karol <kpulawski13@gmail.com>
2024-06-19 10:38:26 +0100

    AuthenticateToken at GET /followers

--- routes/follow.js
+++ routes/follow.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { authenticateToken } = require('../config/jwt');
 
 const followersController = require('../controllers/followersController');
 
-router.get('/followers', followersController.getFollowers);
+router.get('/followers', authenticateToken, followersController.getFollowers);
 router.get('/following', followersController.getFollowing);
 
 module.exports = router;
