8556f403fb4c81f4b1a6b09d85baa85234ef5279
Karol <kpulawski13@gmail.com>
2024-06-23 09:44:55 +0100

    Remove comment

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', async (req, res, next) => {
   res.status(200).json({ message: 'Hello in mine odin-book api' });
 });
