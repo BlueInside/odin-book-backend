8312477aba790dc1cf8c035cddd88203811854c9
Karol <kpulawski13@gmail.com>
2024-06-23 17:50:20 +0100

    implement verify route controller

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,17 +1,7 @@
 const asyncHandler = require('express-async-handler');
 
-const register = asyncHandler(async (req, res, next) => {
-  const registerData = req.body;
-  res.status(201).send(`POST /register/ not implemented`);
+const verify = asyncHandler(async (req, res, next) => {
+  res.status(200).json({ message: 'Valid token', user: req.user });
 });
 
-const login = asyncHandler(async (req, res, next) => {
-  const loginData = req.body;
-  res.status(200).send(`POST /login/ not implemented`);
-});
-
-const logout = asyncHandler(async (req, res, next) => {
-  res.status(200).send(`POST /logout/ not implemented`);
-});
-
-module.exports = { register, login, logout };
+module.exports = { verify };
